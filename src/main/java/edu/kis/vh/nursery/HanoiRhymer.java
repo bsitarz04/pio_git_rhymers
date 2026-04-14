b450709c5e664c2b0ebd874c6dfe8b552dbe29d9
Bartosz <255691@edu.p.lodz.pl>
2026-04-14 18:16:11 +0200

    17. Dodanie dokumentacji Javadoc do klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,27 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa reprezentująca wyliczankę opartą na zasadach wież Hanoi.
+ * Odrzuca liczby większe od aktualnie znajdujących się na szczycie.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * Zwraca całkowitą liczbę odrzuconych prób dodania elementu.
+     * * @return liczba odrzuconych elementów
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Dodaje nowy element do wyliczanki, o ile spełnia on zasady wież Hanoi.
+     * Jeśli podana liczba jest większa niż aktualny element na szczycie,
+     * zostaje odrzucona i licznik odrzuceń wzrasta.
+     * * @param in wartość do dodania do wyliczanki
+     */
     @Override
     public void countIn(final int in) {
         if (!callCheck() && in > peek())
